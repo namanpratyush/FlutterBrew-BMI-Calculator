4da092cc6af3eed378d2546029bd943bf457fc41
Naman <naman.pratyush@nyu.edu>
2020-05-28 22:34:46 -0400

    Updating theme

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,20 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+          primaryColor: Color(0xFF0A0E21),
+          scaffoldBackgroundColor: Color(0xFF0A0E21)),
       home: InputPage(),
     );
   }
 }
+
+//
+//ThemeData(
+//primaryColor: Color(0xFF0A0E21),
+//accentColor: Colors.purple,
+//scaffoldBackgroundColor: Color(0xFF0A0E21),
+//textTheme: TextTheme(bodyText2: TextStyle(color: Colors.white)))
 
 class InputPage extends StatefulWidget {
   @override
@@ -26,8 +36,11 @@ class _InputPageState extends State<InputPage> {
       body: Center(
         child: Text('Body Text'),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+      floatingActionButton: Theme(
+        data: ThemeData(accentColor: Colors.purple),
+        child: FloatingActionButton(
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
